9ea3b4ce9d492c8abda5a7677f18dd2656edceba
Packz Enoch <packz@autistici.org>
2012-05-26 19:25:00 +0200

    Create setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyMsasid',
+    version='0.1.0',
+    author='J. Random Hacker',
+    author_email='jrh@example.com',
+    packages=['pymsasid',],
+    license='LICENSE.txt',
+    description='',
+    long_description=open('readme.txt').read(),
+)
